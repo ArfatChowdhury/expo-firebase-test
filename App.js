1f78db400438c9cc7c69b4711318c83f677130a4
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-10-21 22:02:45 +0600

    done firebase auth

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { auth } from './firebaseConfig';
+
 
 export default function App() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleReg = () => {
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        // console.log(result);
+        console.log('user',result.user);
+        
+
+      })
+      .catch(error => {
+        console.log(error);
+        
+      })
+      setEmail('')
+      setPassword('')
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Please register</Text>
+
+      <TextInput
+        placeholder='Enter your email'
+        onChangeText={setEmail}
+        value={email}
+        style={styles.TextInput}
+      />
+      <TextInput
+        placeholder='Enter your password'
+        onChangeText={setPassword}
+        value={password}
+        style={styles.TextInput}
+        secureTextEntry
+      />
       <StatusBar style="auto" />
+
+      <Button onPress={handleReg} title='Register' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: '10%',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f9f9f9',
+    // alignItems: 'center',
+    // justifyContent: 'flex-end',
+    paddingBottom: 20
   },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 10
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginHorizontal: 20,
+    marginVertical: 10
+
+  }
 });
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -16,4 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+//   export const auth = initializeAuth(app, {
+//     persistence: getReactNativePersistence(AsyncStorage)
+//   });
